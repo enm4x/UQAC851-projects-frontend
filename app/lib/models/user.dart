1415f81cd6605db9d68e1e3e54a481bb63f877d2
scratch <maxime.cpro@outlook.fr>
2021-03-15 19:56:59 -0400

    adding models  user constructor

---
+++ app/lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  int id;
+  String email;
+  String token;
+  String firstName;
+  String lastName;
+
+  User({required this.id, required this.email, required this.token, required this.firstName, required this.lastName});
+}
