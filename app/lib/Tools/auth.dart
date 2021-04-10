702f8519cfc014ef4f097a8c3f0594a4c80d72bd
scratch <maxime.cpro@outlook.fr>
2021-04-10 13:44:41 -0400

    adding a .env parameter for the api connection

--- app/lib/Tools/auth.dart
+++ app/lib/Tools/auth.dart
@@ -1,16 +1,21 @@
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'package:app/models/user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<String> userConnection(String userCredentials, String userPassword) async {
   var client = http.Client();
+  final Digest pwdHash = hashPassword(userPassword);
   try {
-    var response = await client.post(Uri.https("dissidence.dev:9999", "/auth/login"),
-        body: {'email': '$userCredentials', 'password': '$userPassword'});
+    var response = await client.post(Uri.https(env["URL_PROD"].toString(), "/auth/login"),
+        body: {'email': '$userCredentials', 'password': '${pwdHash.toString()}'});
 
     if (response.statusCode == 200) {
       Map res = jsonDecode(response.body);
-      // print(res['token']);
       return res['token'];
     } else {
       throw Exception('Failed to log-in');
@@ -22,9 +27,11 @@ Future<String> userConnection(String userCredentials, String userPassword) async
 
 Future<String> userRegistration(String userCredentials, String userPassword) async {
   var client = http.Client();
+  final Digest pwdHash = hashPassword(userPassword);
+
   try {
-    var response = await client.post(Uri.https("dissidence.dev:9999", "/auth/register"),
-        body: {'email': '$userCredentials', 'password': '$userPassword'});
+    var response = await client.post(Uri.https(env["URL_PROD"].toString(), "/auth/register"),
+        body: {'email': '$userCredentials', 'password': '${pwdHash.toString()}'});
     if (response.statusCode == 201) {
       return response.statusCode.toString();
     } else {
@@ -35,17 +42,44 @@ Future<String> userRegistration(String userCredentials, String userPassword) asy
   }
 }
 
-Future<String> getUserInfo(String userCredentials, String userPassword) async {
+Future<String> getUserInfo(User userObj) async {
   var client = http.Client();
+
   try {
-    var response = await client.post(Uri.https("dissidence.dev:9999", "/auth/register"),
-        body: {'email': '$userCredentials', 'password': '$userPassword'});
-    if (response.statusCode == 201) {
-      return response.statusCode.toString();
+    var response = await client.get(
+      Uri.https(env["URL_PROD"].toString(), "/users/${userObj.email}"),
+      headers: {HttpHeaders.authorizationHeader: "Bearer ${userObj.token}"},
+    );
+
+    if (response.statusCode == 200) {
+      return response.body.toString();
     } else {
-      throw Exception('Failed to register');
+      throw Exception('Can not retrieve user informations');
     }
   } finally {
     client.close();
   }
+}
+
+Future<String> updateUserInfo(User userObj) async {
+  var client = http.Client();
+  try {
+    var response = await client.patch(Uri.https(env["URL_PROD"].toString(), "/users/${userObj.email}"),
+        headers: {HttpHeaders.authorizationHeader: "Bearer ${userObj.token}"},
+        body: {'email': '${userObj.email}', 'first_name': '${userObj.firstName}', 'last_name': '${userObj.lastName}'});
+
+    if (response.statusCode == 204) {
+      return response.statusCode.toString();
+    } else {
+      throw Exception('Failed to update user info');
+    }
+  } finally {
+    client.close();
+  }
+}
+
+Digest hashPassword(String password) {
+  var bytes = utf8.encode(password); // data being hashed
+  var digest = sha256.convert(bytes);
+  return digest;
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:app/screens/loginpage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as Dotenv;
 // import 'package:http/http.dart' as http;
 // import 'dart:async';
 // import 'dart:convert';
 
-void main() {
+void main() async {
+  await Dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
